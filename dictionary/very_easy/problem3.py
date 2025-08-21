38ec59099d26a73301faa39f8feaf4c747c601d2
Sujatabalure <sujatashedole94@gmail.com>
2025-08-21 16:05:15 +0530

    change age value

--- dictionary/very_easy/problem3.py
+++ dictionary/very_easy/problem3.py
@@ -1,1 +1,9 @@
 #Update a value of an existing key (e.g., change "age" from 20 to 25).
+my_dict = {
+    "name" : "sujata",
+    "age" : 30,
+    "city" : "Pune"
+}
+print(my_dict) #before change
+my_dict.update({"age" :25})
+print(my_dict) #after change
