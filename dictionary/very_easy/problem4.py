cc6bb8f418e38049eb2fef66633c43cfd2a77886
Sujatabalure <sujatashedole94@gmail.com>
2025-08-21 16:06:20 +0530

    if statement

--- dictionary/very_easy/problem4.py
+++ dictionary/very_easy/problem4.py
@@ -1,1 +1,8 @@
 #Check if a key exists in a dictionary (e.g., check if "name" exists).
+my_dict = {
+    "name" : "sujata",
+    "age" : 30,
+    "city" : "Pune"
+}
+if "name" in my_dict:
+    print("yes,'name' key exist in dictionary")
