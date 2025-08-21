2d38ff8a75c303bca3f58bc883513fb25b6ffaed
Sujatabalure <sujatashedole94@gmail.com>
2025-08-21 16:11:34 +0530

    len of dic

--- dictionary/easy/problem3.py
+++ dictionary/easy/problem3.py
@@ -1,1 +1,13 @@
 #Merge two dictionaries into one (e.g., using update()).
+my_dict = {
+    "name" : "sujata",
+    "age" : 30,
+    "city" : "Punee"
+}
+my_dau_dict ={"name1" : "Ishani",
+    "age1" : 6,
+    "city1" : "Punee"
+
+}
+my_dict.update(my_dau_dict)
+print(my_dict)
