8cfc45c8586971f12739199d0cd7b2fde85fc0fd
Sujatabalure <sujatashedole94@gmail.com>
2025-08-21 12:42:22 +0530

    pop fuction

--- dictionary/easy/problem1.py
+++ dictionary/easy/problem1.py
@@ -1,1 +1,9 @@
 #Remove a key from a dictionary (using pop()).
+my_dict = {
+    "name" : "sujata",
+    "age" : 30,
+    "city" : "Punee"
+}
+print(my_dict)#before pop
+my_dict.pop("age")
+print(my_dict)
