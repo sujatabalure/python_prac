cd58dc5e092ec5daa2af202dba2ec09f228c0976
Sujatabalure <sujatashedole94@gmail.com>
2025-08-21 16:03:36 +0530

    get function

--- dictionary/very_easy/problem2.py
+++ dictionary/very_easy/problem2.py
@@ -1,1 +1,8 @@
 #Access a value from a dictionary using its key (e.g., get the value of "city").
+my_dict = {
+    "name" : "sujata",
+    "age" : 30,
+    "city" : "Pune"
+}
+x= my_dict.get("city"),
+print(x)
