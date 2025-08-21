8ccb5e107deee7d8edc702c8742379ff22f02ce4
Sujatabalure <sujatashedole94@gmail.com>
2025-08-21 16:48:13 +0530

    copy function

--- dictionary/easy/problem4.py
+++ dictionary/easy/problem4.py
@@ -1,1 +1,15 @@
 #Copy a dictionary (using copy()) and show that modifying the copy doesn’t affect the original.
+my_dict = {
+    "name" : "sujata",
+    "age" : 30,
+    "city" : "Punee"
+}
+my_dau_dict = my_dict.copy()
+print(my_dict)
+print(my_dau_dict)
+
+my_dau_dict.update({ "color" : "white"})
+print(my_dau_dict)
+
+#{"name1" : "Ishani",
+   
