769ad2638e0b76116748576485ce299e03b5fce4
Sujatabalure <sujatashedole94@gmail.com>
2025-08-21 14:17:16 +0530

    create new dic

--- dictionary/very_easy/problem1.py
+++ dictionary/very_easy/problem1.py
@@ -1,2 +1,4 @@
 #Create an empty dictionary and add three key-value pairs (e.g., "name", "age", "city"), then print the dictionary.
 #test
+my_dict ={'name': 'sujata', 'age': 30, 'city': 'Punee'}
+print(my_dict)
