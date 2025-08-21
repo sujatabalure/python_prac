c7ec901cec0ae303253b2b2f91de92870b6732a9
Sujatabalure <sujatashedole94@gmail.com>
2025-08-21 16:07:23 +0530

    loops

--- dictionary/very_easy/problem5.py
+++ dictionary/very_easy/problem5.py
@@ -1,1 +1,8 @@
 #Loop through keys and values in a dictionary and print them one by one.
+my_dict = {
+    "name" : "sujata",
+    "age" : 30,
+    "city" : "Pune"
+}
+for x, y in my_dict.items():
+    print (x,y)
