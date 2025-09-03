4d144c338843106c85ffcf5fd1383ce8d9edce2c
Sujatabalure <sujatashedole94@gmail.com>
2025-09-04 05:06:36 +0530

    word frequency

--- dictionary/medium/probem1.py
+++ dictionary/medium/probem1.py
@@ -1,1 +1,10 @@
 #Count word frequency in a string using a dictionary.
+string = "send a code to git hub a"
+word_freq= {}
+word =string.split()
+for words in word:
+    if words in word_freq :
+        word_freq [words] +=1
+    else:
+        word_freq [words] =1
+print (word_freq)
