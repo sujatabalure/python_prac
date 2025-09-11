6cc3e04b8bfb370ada8172e1e2d4c9c534b3dbd0
Sujatabalure <sujatashedole94@gmail.com>
2025-09-12 03:00:05 +0530

    nested dict

--- dictionary/medium/probem5.py
+++ dictionary/medium/probem5.py
@@ -1,1 +1,18 @@
 #Nested dictionary: Store student data (name, age, grades) inside a dictionary and print specific details.
+my_dict = {
+    "sujata_info":{
+    "name" : "sujata",
+    "age" : 30,
+    },
+
+   "praveen_info":{
+    "name" : "Praveen",
+    "age" : 35,
+    },
+    "ishani_info":{
+    "name" : "ishani",
+    "age" : 5,
+   }
+}
+print(my_dict["ishani_info"])
+
