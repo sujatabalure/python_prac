bcc3442a2418f694a4ff92750119deac509ab1f5
Sujatabalure <sujatashedole94@gmail.com>
2025-09-19 15:53:52 +0530

    maximum value

--- dictionary/medium/probem3.py
+++ dictionary/medium/probem3.py
@@ -1,1 +1,10 @@
 #Find the key with the maximum value in a dictionary.
+my_dict = {
+    "praveen_age" : 36,
+    "Ishani_age"  : 5,
+    "sujata_age" : 30,
+    }
+
+max_age = max(my_dict.items(), key = lambda  item:item[1])
+print(max_age)
+
