1e7046b4d48cd3d5336ceb2c1da63973be192d62
Sujatabalure <sujatashedole94@gmail.com>
2025-09-19 16:17:04 +0530

    invert values

--- dictionary/medium/probem2.py
+++ dictionary/medium/probem2.py
@@ -1,1 +1,10 @@
 #Invert a dictionary (keys → values, values → keys).
+my_dict = {
+    "praveen" : 1,
+    "Ishani"  :2,
+    "sujata" : 3,
+    }
+
+inverted_dict = {value: key for key, value in my_dict.items()}
+print(inverted_dict)
+
