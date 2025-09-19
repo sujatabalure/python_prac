7e28b964aba88ca8f420c7a3ca6a96d3aa6b20b0
Sujatabalure <sujatashedole94@gmail.com>
2025-09-19 15:38:36 +0530

    sorting

--- dictionary/medium/probem4.py
+++ dictionary/medium/probem4.py
@@ -1,1 +1,11 @@
 #Sort a dictionary by values and display the sorted result.
+
+my_dict = {
+    "praveen_age" : 36,
+    "Ishani_age"  : 5,
+    "sujata_age" : 30,
+    }
+
+sorted_dict = dict(sorted(my_dict.items(), key = lambda  item:item[1]))
+print(my_dict)
+print (sorted_dict)
