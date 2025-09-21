6c2c5908b8f42affaf74c91b475816f3e13d76cf
Sujatabalure <sujatashedole94@gmail.com>
2025-09-21 13:36:16 +0530

    changed printing statements and updated comments in code

--- dictionary/easy/problem3.py
+++ dictionary/easy/problem3.py
@@ -1,12 +1,7 @@
 #Merge two dictionaries into one (e.g., using update()).
-my_dict = {
-    "name" : "sujata",
-    "age" : 30,
-    "city" : "Punee"
-}
-my_dau_dict ={"name1" : "Ishani",
-    "age1" : 6,
-    "city" : "Punee"
-}
-my_dict.update(my_dau_dict)
-print(my_dict)
+my_dict = { "name" : "sujata","age" : 30,"city" : "Punee"}#old dictionary
+print("old dictionary", my_dict)#printing old dictionary
+my_dau_dict ={"name1" : "Ishani","age1" : 6, "city" : "Punee"}#new dictionary
+my_dict.update(my_dau_dict)#merging dictionary
+print("new dict", my_dau_dict)#printing new dictionary
+print("merged dict ", my_dict)#printing merged dictionary
