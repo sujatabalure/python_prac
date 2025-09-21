a86a238768bfdf02c7db8c0d29c0210c3c6c52eb
Sujatabalure <sujatashedole94@gmail.com>
2025-09-21 13:39:34 +0530

    changed the print statement as printing length of dictionary with dic name added comments

--- dictionary/easy/problem2.py
+++ dictionary/easy/problem2.py
@@ -1,8 +1,4 @@
 #Get the length of a dictionary using len().
-my_dict = {
-    "name" : "sujata",
-    "age" : 30,
-    "city" : "Punee"
-}
-x= len(my_dict)
-print(x  ," is the length of dictionary", my_dict)
+my_dict = { "name" : "sujata","age" : 30, "city" : "Punee"}#declaring dictionary
+x= len(my_dict)#getting length of dictionary
+print(x  ," is the length of dictionary my_dict", my_dict)#printing length of dictionary
