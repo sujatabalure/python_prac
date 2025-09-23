638dbb8f4ffdc0221cbc46b8daa925b0a9cc3be6
Sujatabalure <sujatashedole94@gmail.com>
2025-09-23 10:36:59 +0530

    added comments to the code

--- dictionary/easy/problem4.py
+++ dictionary/easy/problem4.py
@@ -1,15 +1,12 @@
 #Copy a dictionary (using copy()) and show that modifying the copy doesn’t affect the original.
-my_dict = {
-    "name" : "sujata",
-    "age" : 30,
-    "city" : "Punee"
-}
+my_dict = { "name" : "sujata","age" : 30, "city" : "Punee"}#declaring dictionary
 my_dau_dict = my_dict.copy()
-print(my_dict)
-print(my_dau_dict)
+print(my_dict)#printing original dictionary
+print(my_dau_dict)#printing copied dictionary
 
-my_dau_dict.update({ "color" : "white"})
-print(my_dau_dict)
+my_dau_dict = { "color" : "white"}#modifying copied  dictionary
+print (my_dau_dict)#printing modified dictionary
+print (my_dict)#printing original dictionary
 
 #{"name1" : "Ishani",
    
