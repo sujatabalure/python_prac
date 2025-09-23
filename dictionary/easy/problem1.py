480658e00dc15d0e9e65c40f19684fb1651ef108
Sujatabalure <sujatashedole94@gmail.com>
2025-09-23 11:04:45 +0530

    added comments to the code

--- dictionary/easy/problem1.py
+++ dictionary/easy/problem1.py
@@ -2,4 +2,4 @@
 my_dict = { "name" : "sujata","age" : 30, "city" : "Punee"}#declaring dictionary
 print(my_dict)#before pop printing dict
 my_dict.pop("age")#triming the dict
-print(my_dict)#printing the dictionary after pop statement
+print(my_dict)#printing the dictionary after pop 
