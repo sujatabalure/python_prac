a4e5bf0da9685919fb726ef25737084e161bac67
Sujatabalure <sujatashedole94@gmail.com>
2025-09-23 10:37:23 +0530

    added comments to the code

--- dictionary/easy/problem5.py
+++ dictionary/easy/problem5.py
@@ -1,1 +1,5 @@
 #Create a dictionary with default values using fromkeys() (e.g., keys as subjects, default values "Not Assigned").
+keys =["hindi" , "english","maths"]#initilazing with keys
+default_values = "subject"#initilazing with default values
+my_dict = dict.fromkeys(keys,default_values )#creating dictionary with default values
+print(my_dict)#print dictionary
