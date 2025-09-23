f6d11c13ad17f1a78ac07c65f2f0c4935011081d
Sujatabalure <sujatashedole94@gmail.com>
2025-09-23 10:36:23 +0530

    added comments to the code

--- dictionary/easy/problem1.py
+++ dictionary/easy/problem1.py
@@ -1,9 +1,5 @@
 #Remove a key from a dictionary (using pop()).
-my_dict = {
-    "name" : "sujata",
-    "age" : 30,
-    "city" : "Punee"
-}
-print(my_dict)#before pop
-my_dict.pop("age")
-print(my_dict)
+my_dict = { "name" : "sujata","age" : 30, "city" : "Punee"}#declaring dictionary
+print(my_dict)#before pop printing dict
+my_dict.pop("age")#triming the dict
+print(my_dict)#printing the dictionary after pop statement
