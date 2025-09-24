eb23f40602496fb4217e2363374e40f3df9153f2
Sujatabalure <sujatashedole94@gmail.com>
2025-09-24 12:11:00 +0530

    added correct comments

--- dictionary/easy/problem4.py
+++ dictionary/easy/problem4.py
@@ -1,6 +1,6 @@
 #Copy a dictionary (using copy()) and show that modifying the copy doesn’t affect the original.
 my_dict = { "name" : "sujata","age" : 30, "city" : "Punee"} #declaring dictionary
-my_new_dict = my_dict.copy() #coping above dictionary to new dictionary
+my_new_dict = my_dict.copy()    #coping above dictionary to new dictionary
 print(my_dict)  #printing original dictionary
 print(my_new_dict)  #printing copied dictionary
 
